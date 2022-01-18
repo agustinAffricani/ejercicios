b9fe9cd391ae02a74b8a1fca6c8b6693d7f71156
agustinAffricani <agustin.affricani@gmail.com>
2022-01-18 20:12:49 -0300

    calculadora iva

--- calcular_iva.php
+++ calcular_iva.php
@@ -7,19 +7,26 @@
     $resPrecioConIva = 0;
     $resPrecioSinIva = 0;
     $resIvaCantidad = 0;
+    $mensaje = "";
 
     if($_POST){
         $iva = $_REQUEST["lstIva"];
         $resPrecioConIva = $_REQUEST["txtIva"];
         $resPrecioSinIva = $_REQUEST["txtSinIva"];
 
-        if($resPrecioConIva != ""){
-            if($iva == 27){
-                
-            }
+        if($resPrecioConIva != "" && $resPrecioSinIva == ""){
+                $resPrecioSinIva = $resPrecioConIva/($iva/100+1);
+                $resIvaCantidad = $resPrecioConIva - $resPrecioSinIva;
+        } elseif($resPrecioConIva == "" && $resPrecioSinIva != ""){
+                $resPrecioConIva = $resPrecioSinIva*($iva/100+1);
+                $resIvaCantidad = $resPrecioConIva - $resPrecioSinIva;
+        } elseif(($resPrecioConIva == "" && $resPrecioSinIva == "") || ($resPrecioConIva != "" && $resPrecioSinIva != "")){
+            $resPrecioConIva = 0;
+            $resPrecioSinIva = 0;
+            $resIvaCantidad = 0;
+            $mensaje = "Debes completar uno de los campos";
         }
     }
-
 ?>
 
 <!DOCTYPE html>
@@ -34,12 +41,12 @@
 <body>
     <main class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 my-3">
                 <h1>Calculadora de IVA</h1>
             </div>
         </div>
         <div class="row">
-            <div class="col-6">
+            <div class="col-4">
                 <form method="POST" action="">
                     <div>
                     <label for="txtValorIva">IVA
@@ -51,38 +58,44 @@
                     </label>
                     </div>
                     <div>
-                        <label for="txtIva">Precio sin IVA:</label>
-                        <input name="txtIva" id="txtIva" class="form-control">
+                        <label for="txtSinIva">Precio sin IVA:</label>
+                        <input name="txtSinIva" id="txtSinIva" class="form-control">
                     </div>
                     <div>
-                        <label for="txtSinIva">Precio con IVA:</label>
-                        <input name="txtSinIva" id="txtSinIva" class="form-control">
+                        <label for="txtIva">Precio con IVA:</label>
+                        <input name="txtIva" id="txtIva" class="form-control">
                     </div>
                     <div>
                        <button type="submit" class="btn btn-primary my-3">Calcular</button>
                     </div>
                 </form>
+                <?php
+                    if(isset($mensaje) && $mensaje != ""):{
+                ?>
+                        <div class="alert alert-danger" role="alert"><?php echo $mensaje; ?></div>
+                <?php
+                    }endif;
+                ?>
             </div>
-            <div class="col-6">
-            <table class="table table-hover border">
+            <div class="col-4">
+            <table class="table table-hover border mt-3">
                     <tr>
                         <th>IVA:</th>
                         <td>$<?php echo $iva; ?></td>
                     </tr>
                     <tr>
                         <th>Pecio sin IVA:</th>
-                        <td>$<?php echo $resPrecioSinIva; ?></td>
+                        <td>$<?php echo number_format(floatval($resPrecioSinIva),2,",","."); ?></td>
                     </tr>
                     <tr>
                         <th>Pecio con IVA:</th>
-                        <td>$<?php echo $resPrecioConIva; ?></td>
+                        <td>$<?php echo number_format(floatval($resPrecioConIva),2,",","."); ?></td>
                     </tr>
                     <tr>
                         <th>IVA cantidad:</th>
-                        <td>$<?php echo $resIvaCantidad; ?></td>
+                        <td>$<?php echo number_format(floatval($resIvaCantidad),2,",","."); ?></td>
                     </tr>
                 </table>
-
             </div>
         </div>
     </main>
